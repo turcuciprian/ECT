00b3207181a21d6e3a4345dbaf32ab752eb48771
Ciprian Turcu <turcuciprian1@gmail.com>
2018-03-13 20:01:10 +0200

    webpack config again

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const webpack = require('webpack');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: ['./app.js']
+  },
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'commons',
+    filename: 'commons.js',
+    minChunks: 2,
+  }),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: {
+        baseDir: ['./']
+      },
+    })
+    // ,
+    // new UglifyJsPlugin()
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './'),
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          },
+        }],
+      },
+      { test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query:{
+          presets: ['react'],
+          compact:false
+        },
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ],
+  },
+};
